177170bd1268da90e476c4a2bc94c14d8f62e9f6
Radu Stefan <radustefan3010@gmail.com>
2025-12-19 15:13:46 +0200

    Update project folder path

--- ShopNow/app/src/main/java/com/example/shopnow/features/products/ProductListViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shopnow.features.products
-
-import com.example.shopnow.data.Product
-
-sealed interface ProductListUiState {
-    data object Loading : ProductListUiState
-    data class Data(val items: List<Product>) : ProductListUiState
-    data class Error(val message: String) : ProductListUiState
-}
